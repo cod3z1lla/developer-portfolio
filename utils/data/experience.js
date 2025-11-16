6a53504bd60ae33a19862fb748c9182f724cb4a9
Muhammad Shahzaib <111436809+cod3z1lla@users.noreply.github.com>
2025-11-16 14:07:36 +0500

    Update experiences with new roles and durations

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,14 +1,38 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Mobile App Developer Intern',
-    company: "LogicAspire",
-    duration: "(Feb 2024 - Present)"
+    title: 'Software Engineer',
+    company: "Freelance",
+    duration: "(Jan 2025 - Present)"
   },
   {
     id: 2,
-    title: "Web Developer Intern",
-    company: "Bytewise ltd.",
-    duration: "(Jan 2023 - May 2023)"
+    title: 'Mobile Application Developer',
+    company: "LogicAspire",
+    duration: "(Aug 2024 - Present)"
+  },
+  {
+    id: 3,
+    title: 'Software Engineering Fellow',
+    company: "Headstarter AI",
+    duration: "(Jul 2024 - Sep 2024)"
+  },
+  {
+    id: 4,
+    title: 'Mobile App Developer Intern',
+    company: "Bytewise Limited",
+    duration: "(Jun 2024 - Sep 2024)"
+  },
+  {
+    id: 5,
+    title: 'Mobile App Developer Intern',
+    company: "LogicAspire",
+    duration: "(Feb 2024 - Jul 2024)"
+  },
+  {
+    id: 6,
+    title: 'Web Development Intern',
+    company: "Bytewise Limited",
+    duration: "(Mar 2023 - Jun 2023)"
   },
 ]
