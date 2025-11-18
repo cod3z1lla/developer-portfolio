6794cfdbe25708e5249b10a2adaf09a3ed865d86
Muhammad Shahzaib <shahzaib3742@gmail.com>
2025-11-18 19:06:57 +0500

    Remove email handling logic from contact form component

--- app/components/homepage/contact/contact-without-captcha.jsx
+++ app/components/homepage/contact/contact-without-captcha.jsx
@@ -1,67 +1,21 @@
 "use client";
 // @flow strict
-import { isValidEmail } from "@/utils/check-email";
-import emailjs from "@emailjs/browser";
-import axios from "axios";
-import { useState } from "react";
-import { TbMailForward } from "react-icons/tb";
-import { toast } from "react-toastify";
 
 function ContactWithoutCaptcha() {
-  const [error, setError] = useState({ email: false, required: false });
-  const [userInput, setUserInput] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const checkRequired = () => {
-    if (userInput.email && userInput.message && userInput.name) {
-      setError({ ...error, required: false });
-    }
-  };
-
-  const handleSendMail = async (e) => {
-    e.preventDefault();
-    if (!userInput.email || !userInput.message || !userInput.name) {
-      setError({ ...error, required: true });
-      return;
-    } else if (error.email) {
-      return;
-    } else {
-      setError({ ...error, required: false });
-    }
-
-    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
-    const options = { publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY };
-
-    try {
-      const res = await emailjs.send(serviceID, templateID, userInput, options);
-      const teleRes = await axios.post(
-        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
-        userInput
-      );
-
-      if (res.status === 200 || teleRes.status === 200) {
-        toast.success("Message sent successfully!");
-        setUserInput({
-          name: "",
-          email: "",
-          message: "",
-        });
-      }
-    } catch (error) {
-      toast.error(error?.text || error);
-    }
-  };
-
   return (
     <div className="">
       <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">
         Contact with me
       </p>
-
+      <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
+        <p className="text-sm text-[#d3d8e8]">
+          {
+            "If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."
+          }
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ContactWithoutCaptcha;
